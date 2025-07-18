f4e8c356add2d092df80e42b039755c72453113c
Roney Moon <112367082+roneymoon@users.noreply.github.com>
2025-07-18 19:05:14 +0530

    added a file

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,10 @@
+# Given a string s, find the length of the longest 
+# substring without duplicate characters.
+
+# s = "abcabcbb" -> longest substring without repeating = "abc"
+# As the longest substring is "abc" hence return length 3
+
+# s = "pwwkew" -> longest substring withour repeating = "wke"
+# Hence the longest string length will be 3
+
+
